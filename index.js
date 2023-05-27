9e60c110862f2543ba75968577d9060cf2aa118c
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2023-05-27 12:30:13 -0300

    crud finalizado!

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import express from 'express'
 import admin from 'firebase-admin'
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express()
-
+app.use(express.json())
 
 
 admin.initializeApp({
   credential: admin.credential.cert("serviceAccountKey.json")
 });
 
-app.get('/listOfPasswords', (request, response) => {
+app.get('/listOfPasswords', (request, res) => {
     console.log('Get List of Passwords')
     admin.firestore()
         .collection('passwords')
@@ -20,9 +21,39 @@ app.get('/listOfPasswords', (request, response) => {
                 uid: doc.id
             }))
 
-            response.json(passwords)
+            res.json(passwords)
         })
-    
+})
+
+app.post('/newPassword', (req, res) => {
+    // console.log(req)
+    const password = req.body
+    console.log(password)
+    const id = uuidv4()
+    admin.firestore()
+        .collection('passwords')
+        .doc(id)
+        .set(password)
+    res.status(200).send("Senha adicionada")
+})
+
+app.put('/password/update/:id', (req, res) => {
+    const id = req.params.id
+    const password = req.body
+    admin.firestore()
+        .collection('passwords')
+        .doc(id)
+        .set(password)
+    res.status(200).send("Senha atualizada")
+})
+
+app.delete('/password/delete/:id', (req, res) => {
+    const id = req.params.id
+    admin.firestore()
+        .collection('passwords')
+        .doc(id)
+        .delete()
+    res.status(200).send("Senha deletada com sucesso!")
 })
 
 
